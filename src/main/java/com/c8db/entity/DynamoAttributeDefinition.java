e920cd16d53a66e3d37b9a81cdcfa42992274fa6
Stoyan <Stoyan.Tchervenkov@macrometa.com>
2022-03-09 09:28:16 +0200

    Introduced DynamoAttributeDefinition.java

---
+++ src/main/java/com/c8db/entity/DynamoAttributeDefinition.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Macrometa Corp All rights reserved.
+ */
+
+package com.c8db.entity;
+
+import lombok.Data;
+
+@Data
+public class DynamoAttributeDefinition {
+
+    private String  AttributeName;
+    private String  AttributeType;
+}
