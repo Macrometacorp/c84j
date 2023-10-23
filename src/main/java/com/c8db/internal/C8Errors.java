a8312f3e34e2053730b6f48f925799c764bcbe79
Dmytro Lazarenko <dmytro.lazarenko@macrometa.com>
2023-10-23 16:25:36 -0700

    Fix ERROR_STREAM_ALREADY_EXISTS to 1210

--- src/main/java/com/c8db/internal/C8Errors.java
+++ src/main/java/com/c8db/internal/C8Errors.java
@@ -28,7 +28,7 @@ public final class C8Errors {
     public static final Integer ERROR_C8_DATA_SOURCE_NOT_FOUND = 1203;
     public static final Integer ERROR_C8_DATABASE_NOT_FOUND = 1228;
     public static final Integer ERROR_GRAPH_NOT_FOUND = 1924;
-    public static final Integer ERROR_STREAM_ALREADY_EXISTS = 100017;
+    public static final Integer ERROR_STREAM_ALREADY_EXISTS = 1210;
     public static final Integer ERROR_STREAM_NOT_FOUND = 100016;
     public static final Integer ERROR_COLLECTION_ALREADY_EXISTS = 1207;
 
